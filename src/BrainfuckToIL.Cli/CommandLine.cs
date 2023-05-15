e6ddc9a59ed98fb0b69d2f1eedb71c3762d3f619
thinker227 <thinker0227@gmail.com>
2023-05-15 19:21:05 +0200

    Add basic CLI

---
+++ src/BrainfuckToIL.Cli/CommandLine.cs
@@ -0,0 +1,68 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+
+namespace BrainfuckToIL.Cli;
+
+public static class CommandLine
+{
+    public static Parser GetParser(RootCommand rootCommand)
+    {
+        var builder = new CommandLineBuilder(rootCommand);
+        
+        builder.UseDefaults();
+        
+        return builder.Build();
+    }
+
+    public static RootCommand GetRootCommand(Action<FileInfo, FileSystemInfo> handler)
+    {
+        RootCommand rootCommand = new()
+        {
+            Name = "BFtoIL",
+            Description = "Simplistic Brainfuck to IL compiler."
+        };
+
+        var sourceFileArgument = SourceFileArgument();
+        rootCommand.AddArgument(sourceFileArgument);
+
+        var outputDestinationArgument = OutputDestinationArgument();
+        rootCommand.AddArgument(outputDestinationArgument);
+
+        rootCommand.SetHandler(
+            handler,
+            sourceFileArgument,
+            outputDestinationArgument);
+
+        return rootCommand;
+    }
+
+    private static Argument<FileInfo> SourceFileArgument()
+    {
+        Argument<FileInfo> argument = new()
+        {
+            Name = "source",
+            Description = "The source file to compile."
+        };
+        argument.LegalFilePathsOnly();
+        argument.ExistingOnly();
+
+        return argument;
+    }
+
+    private static Argument<FileSystemInfo> OutputDestinationArgument()
+    {
+        Argument<FileSystemInfo> argument = new()
+        {
+            Name = "output",
+            Description = "The output destination for the compiled DLL file. " +
+                          "If the provided value is a directory then " +
+                          "the output DLL file will be located in the specified directory " +
+                          "and use the file name of the source file."
+        };
+        argument.LegalFilePathsOnly();
+        argument.ExistingOnly();
+        
+        return argument;
+    }
+}
--- src/BrainfuckToIL.Cli/Program.cs
+++ src/BrainfuckToIL.Cli/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine.Parsing;
+using BrainfuckToIL.Cli;
+
+var rootCommand = CommandLine.GetRootCommand((sourceFile, destination) =>
+{
+    Console.WriteLine($"""
+    Input file: {sourceFile.FullName}
+    Output destination: {destination}
+    """);
+});
+
+var parser = CommandLine.GetParser(rootCommand);
+parser.Invoke(args);
