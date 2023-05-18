87368a6f785975be06ad6cd6f9964e5fc7151812
thinker227 <thinker0227@gmail.com>
2023-05-18 19:07:07 +0200

    Add EmitHeader

--- src/BrainfuckToIL/InstructionEmitter.cs
+++ src/BrainfuckToIL/InstructionEmitter.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Metadata;
+﻿using System.Collections.Immutable;
+using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 
 namespace BrainfuckToIL;
@@ -13,6 +14,10 @@ internal sealed class InstructionEmitter
     private readonly InstructionEncoder il;
     private readonly LocalVariablesEncoder locals;
     private readonly Types types;
+
+    private const int MemorySize = 30_000;
+    private const int MemorySlot = 0;
+    private const int DataPointerSlot = 1;
 
     private InstructionEmitter(
         IReadOnlyList<Instruction> instructions,
@@ -36,9 +41,43 @@ internal sealed class InstructionEmitter
         Types types)
     {
         var emitter = new InstructionEmitter(instructions, metadata, il, locals, types);
+        
+        emitter.EmitHeader();
         emitter.Emit();
     }
 
+    /// <summary>
+    /// Emits the header of the main method which doesn't vary depending on instructions.
+    /// This includes allocating the memory array and setting up locals.
+    /// </summary>
+    private void EmitHeader()
+    {
+        // Add a byte[] variable.
+        locals.AddVariable().Type().Array(
+            elementType => elementType.Byte(),
+            shape => shape.Shape(
+                1,
+                // The array will always contain 30000 elements.
+                ImmutableArray.Create(1),
+                ImmutableArray.Create(MemorySize)));
+        
+        // Add an int variable.
+        locals.AddVariable().Type().Int32();
+        
+        // Create an array of 30,000 bytes and store it into a local variable. 
+        il.LoadConstantI4(MemorySize);
+        il.OpCode(ILOpCode.Newarr);
+        il.Token(types.Byte);
+        il.StoreLocal(MemorySlot);
+
+        // Initialize the data pointer local variable to 0.
+        il.LoadConstantI4(0);
+        il.StoreLocal(DataPointerSlot);
+    }
+
+    /// <summary>
+    /// Emits the main part of the body based on <see cref="instructions"/>.
+    /// </summary>
     private void Emit()
     {
         il.OpCode(ILOpCode.Nop);
