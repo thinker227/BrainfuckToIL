5ea5736cd54ea3cff29de158aaee235aaf1da3ab
thinker227 <thinker0227@gmail.com>
2023-05-19 15:16:23 +0200

    Create Files.cs

---
+++ src/BrainfuckToIL.Cli/Files.cs
@@ -0,0 +1,48 @@
+﻿namespace BrainfuckToIL.Cli;
+
+/// <summary>
+/// Manages files for the CLI.
+/// </summary>
+internal static class Files
+{
+    /// <summary>
+    /// Gets or creates an output file.
+    /// </summary>
+    /// <param name="inputFile">The input file.</param>
+    /// <param name="outputDestination">The output destination, or <see langword="null"/>.</param>
+    /// <param name="outputExe">Whether to output an exe file or not.</param>
+    /// <returns>The created output file, or <paramref name="inputFile"/> if it already exists.</returns>
+    public static FileInfo GetOrCreateOutputFile(
+        FileInfo inputFile,
+        FileSystemInfo? outputDestination,
+        bool outputExe) =>
+        outputDestination switch
+        {
+            FileInfo { Exists: true } file => file,
+            
+            FileInfo file => CreateFile(file),
+            
+            DirectoryInfo dir => ToDirectory(inputFile, dir, outputExe),
+            
+            null => ToDirectory(inputFile, inputFile.Directory, outputExe),
+            
+            _ => throw new UnreachableException()
+        };
+
+    private static FileInfo CreateFile(FileInfo file)
+    {
+        File.Create(file.FullName);
+        return new(file.FullName);
+    }
+
+    private static FileInfo ToDirectory(FileInfo inputFile, DirectoryInfo outputDirectory, bool outputExe)
+    {
+        var inputName = Path.GetFileNameWithoutExtension(inputFile.Name);
+        var outputExtension = outputExe ? ".exe" : ".dll";
+        var outputName = inputName + outputExtension;
+        var outputPath = Path.Combine(outputDirectory.FullName, outputName);
+        
+        File.Create(outputPath);
+        return new(outputPath);
+    }
+}
--- src/BrainfuckToIL.Cli/Program.cs
+++ src/BrainfuckToIL.Cli/Program.cs
@@ -3,33 +3,7 @@ using BrainfuckToIL.Cli;
 
 var rootCommand = CommandLine.GetRootCommand((sourceFile, destination, outputExe) =>
 {
-    FileInfo outputFile;
-    switch (destination)
-    {
-    case FileInfo { Exists: true } file:
-        outputFile = file;
-        break;
-    
-    case FileInfo:
-        File.Create(destination.FullName);
-        outputFile = new(destination.FullName);
-        break;
-    
-    case DirectoryInfo:
-        {
-            var inputName = Path.GetFileNameWithoutExtension(sourceFile.Name);
-            var outputExtension = outputExe ? ".exe" : ".dll";
-            var outputName = inputName + outputExtension;
-            var outputPath = Path.Combine(sourceFile.DirectoryName, outputName);
-            
-            File.Create(outputPath);
-            outputFile = new(outputPath);
-            break;
-        }
-    
-    default:
-        throw new UnreachableException();
-    }
+    var outputFile = Files.GetOrCreateOutputFile(sourceFile, destination, outputExe);
 
     Console.WriteLine($"""
     Source file: {sourceFile.FullName}
