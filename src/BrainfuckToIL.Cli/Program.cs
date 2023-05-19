531291a769ec0ee449c8ee33f1a163d2f8b2dfe4
thinker227 <thinker0227@gmail.com>
2023-05-19 20:57:41 +0200

    Update Program.cs

--- src/BrainfuckToIL.Cli/Program.cs
+++ src/BrainfuckToIL.Cli/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System.CommandLine.Parsing;
+using System.Reflection;
 using BrainfuckToIL;
 using BrainfuckToIL.Cli;
 
-var rootCommand = CommandLine.GetRootCommand((sourceFile, destination, outputExe) =>
+var rootCommand = CommandLine.GetRootCommand(Compile, Run);
+
+var parser = CommandLine.GetParser(rootCommand);
+parser.Invoke(args);
+
+
+
+static void Compile(FileInfo sourceFile, FileSystemInfo? destination, bool outputExe)
 {
     var outputFile = Files.GetOrCreateOutputFile(sourceFile, destination, outputExe);
 
@@ -13,7 +21,23 @@ var rootCommand = CommandLine.GetRootCommand((sourceFile, destination, outputExe
 
     using var outputStream = outputFile.OpenWrite();
     Emitter.Emit(instructions, outputStream);
-});
+}
 
-var parser = CommandLine.GetParser(rootCommand);
-parser.Invoke(args);
+static void Run(FileInfo sourceFile)
+{
+    var source = File.ReadAllText(sourceFile.FullName);
+    var instructions = BrainfuckToIL.Parser.Parse(source);
+
+    var stream = new MemoryStream();
+    Emitter.Emit(instructions, stream);
+    var bytes = stream.ToArray();
+
+    var assembly = Assembly.Load(bytes);
+    var programType = assembly.GetType("$<Program>") ?? throw new InvalidOperationException(
+        "Could not find or load type $<Program>.");
+    var mainMethod = programType.GetMethod("$<Main>") ?? throw new InvalidOperationException(
+        "Could not find or load method $<Main>.");
+    var main = mainMethod.CreateDelegate<Action>();
+    
+    main();
+}
