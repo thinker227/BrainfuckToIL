40c7f13a903584c86acae7d0bcfaebc018cb9f01
thinker227 <thinker0227@gmail.com>
2023-05-20 19:24:46 +0200

    Add InstructionExtensions and ParseResult.Errors

---
+++ src/BrainfuckToIL/InstructionExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace BrainfuckToIL;
+
+/// <summary>
+/// Extensions for <see cref="Instruction"/>.
+/// </summary>
+public static class InstructionExtensions
+{
+    /// <summary>
+    /// Flattens a sequence of instructions, producing a flat sequence of instructions
+    /// without nested instructions. 
+    /// </summary>
+    /// <param name="instructions">The instructions to flatten.</param>
+    public static IEnumerable<Instruction> Flatten(this IEnumerable<Instruction> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            yield return instruction;
+
+            if (instruction is not Instruction.Loop loop) continue;
+
+            foreach (var loopInstruction in loop.Instructions) yield return loopInstruction;
+        }
+    }
+}
--- src/BrainfuckToIL/ParseResult.cs
+++ src/BrainfuckToIL/ParseResult.cs
@@ -6,4 +6,11 @@ namespace BrainfuckToIL;
 /// The result of a parse operation.
 /// </summary>
 /// <param name="Instructions">The parsed instructions.</param>
-public readonly record struct ParseResult(ImmutableArray<Instruction> Instructions);
+public readonly record struct ParseResult(ImmutableArray<Instruction> Instructions)
+{
+    /// <summary>
+    /// The errors produced by the parser.
+    /// </summary>
+    public IEnumerable<Error> Errors =>
+        Instructions.Flatten().SelectMany(i => i.Errors);
+}
