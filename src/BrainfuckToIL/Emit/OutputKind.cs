677b72d094b160462c6cc1ee7fa5ed7c1cdfe7c9
thinker227 <thinker0227@gmail.com>
2023-05-25 14:59:20 +0200

    Add docs to OutputKind

--- src/BrainfuckToIL/Emit/OutputKind.cs
+++ src/BrainfuckToIL/Emit/OutputKind.cs
@@ -1,7 +1,16 @@
 ﻿namespace BrainfuckToIL.Emit;
 
+/// <summary>
+/// Represents kinds of emit output.
+/// </summary>
 public enum OutputKind
 {
+    /// <summary>
+    /// Emit an executable.
+    /// </summary>
     Executable,
+    /// <summary>
+    /// Emit a DLL.
+    /// </summary>
     Dll
 }
