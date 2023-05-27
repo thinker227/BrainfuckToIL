c2c61d6eea3d242a829471b0208030dbf7c65134
thinker227 <thinker0227@gmail.com>
2023-05-27 12:42:53 +0200

    Add more program delegates

--- src/BrainfuckToIL/BrainfuckMethod.cs
+++ src/BrainfuckToIL/BrainfuckMethod.cs
@@ -1,12 +1,35 @@
 ﻿namespace BrainfuckToIL;
 
+// These are equivalent to Action, Action<T>, Func<T>, and Func<T1, TResult>
+// but they serve to distinguish a Brainfuck program delegate from other more general delegates.
+
 /// <summary>
 /// Represents a method which executes a piece of Brainfuck code.
 /// </summary>
 public delegate void BrainfuckMethod();
 
 /// <summary>
-/// Represents a method which executes a piece of Brainfuck code and returns a value.
+/// Represents a method which executes a piece of Brainfuck code
+/// using a specified input.
 /// </summary>
-/// <typeparam name="T">The type of the value the method returns.</typeparam>
-public delegate T BrainfuckMethod<out T>();
+/// <param name="input">The input to the program.</param>
+/// <typeparam name="TIn">The type of the input to the program.</typeparam>
+public delegate void BrainfuckMethod<in TIn>(TIn input);
+
+/// <summary>
+/// Represents a function which executes a piece of Brainfuck code
+/// and returns the output of the program.
+/// </summary>
+/// <returns>The output of the program.</returns>
+/// <typeparam name="TOut">The type of the output of the program.</typeparam>
+public delegate TOut BrainfuckFunction<out TOut>();
+
+/// <summary>
+/// Represents a function which executes a piece of Brainfuck code
+/// using a specified input and returns the output of the program.
+/// </summary>
+/// <param name="input">The input to the program.</param>
+/// <typeparam name="TIn">The type of the input to the program.</typeparam>
+/// <typeparam name="TOut">The type of the output of the program.</typeparam>
+/// <returns>The output of the program.</returns>
+public delegate TOut BrainfuckFunction<in TIn, out TOut>(TIn input);
